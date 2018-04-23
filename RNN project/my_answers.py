152beb6989a5b5e06a1ebc4a50078a7449612c57
IslamHisham <30858389+IslamHisham@users.noreply.github.com>
2018-04-23 23:23:06 +0200

    Add files via upload

---
+++ RNN project/my_answers.py
@@ -0,0 +1,63 @@
+import numpy as np
+
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+import keras
+
+
+# TODO: fill out the function below that transforms the input series 
+# and window-size into a set of input/output pairs for use with our RNN model
+def window_transform_series(series,window_size):
+    X=[]
+    y=[]
+    for slider in range(0,len(series)):
+        if slider+window_size < len(series):
+            X.append(list(series[slider:slider+window_size]))
+            y.append([series[slider+window_size]])
+    X = np.asarray(X)
+    X.shape = (np.shape(X)[0:2])
+    y = np.asarray(y)
+    y.shape = (len(y),1)
+    return X,y
+
+# TODO: build an RNN to perform regression on our time series input/output data
+def build_part1_RNN(window_size):
+    model = Sequential()
+    model.add(LSTM(5, input_shape=(window_size, 1)))
+    model.add(Dense(1))
+    return model
+
+
+### TODO: return the text input with only ascii lowercase and the punctuation given below included.
+def cleaned_text(text):
+    result=""
+    for word in text:
+        for char in word:
+            if char not in [' ', '!', ',', '.',':', ';', '?', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']:
+                continue
+            result+=char
+    return result
+
+### TODO: fill out the function below that transforms the input text and window-size into a set of input/output pairs for use with our RNN model
+def window_transform_text(text,window_size,step_size):
+    # containers for input/output pairs
+    inputs = []
+    outputs = []
+    pointer = 0
+    
+    # Goes from window_size until the end, and pick previous characters
+    for slider in range(window_size, len(text), step_size):
+        inputs.append(text[pointer:slider])
+        outputs.append(text[slider])
+        pointer+= step_size
+    
+    return inputs,outputs
+
+# TODO build the required RNN model: 
+# a single LSTM hidden layer with softmax activation, categorical_crossentropy loss 
+def build_part2_RNN(window_size, num_chars):
+    model = Sequential()
+    model.add(LSTM(200, input_shape=(window_size, num_chars)))
+    model.add(Dense(num_chars, activation='softmax'))
+    return model
